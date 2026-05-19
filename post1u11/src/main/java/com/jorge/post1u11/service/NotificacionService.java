d247e9bcb6f685c43bb86b10444e014c57ca0b8d
JorgeRicoP <jorgeandresricopinilla@hotmail.com>
2026-05-18 19:18:03 -0500

    refactor: aplicar extract method y value objects

---
+++ post1u11/src/main/java/com/jorge/post1u11/service/NotificacionService.java
@@ -0,0 +1,14 @@
+package com.jorge.post1u11.service;
+
+import com.jorge.post1u11.valueobject.DatosCliente;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificacionService {
+
+    public void notificarPedido(DatosCliente cliente, boolean urgente) {
+
+        System.out.println("Enviando email a: " + cliente.getEmail());
+        System.out.println("Pedido urgente: " + urgente);
+    }
+}
---
+++ post1u11/src/main/java/com/jorge/post1u11/valueobject/Direccion.java
@@ -0,0 +1,27 @@
+package com.jorge.post1u11.valueobject;
+
+public class Direccion {
+
+    private final String calle;
+    private final String ciudad;
+    private final String codigoPostal;
+
+    public Direccion(String calle, String ciudad, String codigoPostal) {
+
+        this.calle = calle;
+        this.ciudad = ciudad;
+        this.codigoPostal = codigoPostal;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public String getCodigoPostal() {
+        return codigoPostal;
+    }
+}
