7d3aae2c7ddd6b6e16c5f58f895c90a7283a3a94
JorgeRicoP <jorgeandresricopinilla@hotmail.com>
2026-05-18 19:13:19 -0500

    chore: crear estructura inicial del proyecto

---
+++ post1u11/src/main/java/com/jorge/post1u11/Post1u11Application.java
@@ -0,0 +1,13 @@
+package com.jorge.post1u11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Post1u11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Post1u11Application.class, args);
+	}
+
+}
---
+++ post1u11/src/test/java/com/jorge/post1u11/Post1u11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorge.post1u11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Post1u11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
